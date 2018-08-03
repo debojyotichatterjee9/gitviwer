c030ec7d5c5e0d23578c507760beb8db805fdca0
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2018-08-03 18:11:21 +0530

    authentication successful

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import logo from './logo.svg';
 import firebase from 'firebase';
 import './App.css';
 import View from "./View.js";
-import Login from './Login.js';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth.js";
+
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC2pq8pKov9QQUmWuQ6yKf3sSumJES_bHo",
+    authDomain: "gitviewer-df26d.firebaseapp.com"
+})
 
 class App extends Component {
   
 state ={
+  isSignedIn: false,
   user: {}
+}
+uiConfig = {
+  signInFlow: "popup",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccess: () => false
+  }
+}
+
+componentDidMount = () =>{
+  
+  firebase.auth().onAuthStateChanged(user => {
+    this.setState({isSignedIn: !!user })
+  })
 }
 
 getUser = () =>{
@@ -39,27 +65,35 @@ getUser = () =>{
   
     
 }
-authenticate= provider =>{
-       
-};
+
   render() {
-
-    return (<Login/>);
-
+  
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">GitHub Viewer</h1>
         </header>
-        <p className="App-intro">
+        {this.state.isSignedIn ? (
+         <span className="App-intro">       
+        <p>
         <code>To get started, enter <b>a profile name</b> below and click the View button.</code>
         </p>
         <input type="text" placeholder="Type here..." ref="name"/>
         <button onClick={this.getUser}>View</button>
+        <button onClick={() => firebase().auth.signOut()}>Sign Out</button>
         <View user={this.state.user} />
+        </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+          // <div>Not Signed in</div>
+        )}
       </div>
     );
+    
   }
 }
 
